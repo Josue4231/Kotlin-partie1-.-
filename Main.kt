e4f67647fc1601749a360bd88b8dde46d939aa24
Josue4231 <josuekialengela@gmail.com>
2025-02-03 11:08:06 +0100

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,69 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+/*fun main() {
+    val x = 14
+    val y = 8
+    var name : Int = 12
+    val firtName= "Josue"
+    val age : Int = 20
+    val  née = 2005
+
+    println(x * 2 + y * 4 + name )
+    println(" Je m'appelle " + firtName + " j'ai "
+    + age +" ans " + " et je suis née les 10 Janvier " + née  )
+}
+*/
+
+/*fun main(){ //nombre et chiffre
+    val num1 : Double = 10.99
+    val num2 = 10.04F
+    val num3 = 5.9E5F
+    val num4 = 7E6
+    println(num1)
+    println(num2)
+    println(num3)
+    println(num4)
+
+}*/
+
+/*fun main(){
+    val jojo : Char = 'A'
+    val jo : Int = 25
+    var getting = "Itmepabloescorbar"
+    val riv = "Josue"
+    val rive = "Tazi"
+    println(getting[4])
+    println(getting.length)
+    println(getting.toUpperCase())
+    println(getting.toLowerCase())
+    println("it me $riv $rive" )
+    println(jojo+jo)
+}*/
+
+/*fun main() {
+    var n: Int = 10
+    println(n > 14)
+}
+*/
+
+/*fun main() {
+    val temp = 17
+    if (temp < 18) {
+        println("il fait sombre")
+    }
+    else if  (temp > 18){
+        println("c'est aasez lumineux ")
+    }
+    else {
+        println("Pas bon")
+    }
+
+}
+*/
+fun main(){
+    val time = 10
+    var getting = if (time==10) "God" else if (time<10) "Bad" else "Sorry"
+    println(getting)
+}
+
