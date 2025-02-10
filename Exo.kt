d45b118a6ae60a90dc2c3da8c069a0873256ce6c
Josue4231 <josuekialengela@gmail.com>
2025-02-10 10:05:51 +0100

    Add files via upload

--- Exo.kt
+++ Exo.kt
@@ -1,4 +1,4 @@
-/*fun main(){     //Exo1
+/*fun main(){     //Exo1.1
     println("Entrer un nom")
     val name = readln()
     var x=10
@@ -40,7 +40,7 @@
     }
 }*/
 
-/*fun main(){ //exo 2
+/*fun main(){ //exo 1.2
     println("Entrer un nombre")
     val xInput= readln()
     val x = xInput.toIntOrNull()
@@ -56,7 +56,7 @@
     }
 }*/
 
-/*fun main(){ //exo3
+/*fun main(){ //exo1.3
     println("Entrer votre année")
     val annee = readln().toInt()
 
@@ -73,7 +73,7 @@
     }
 }*/
 
-fun main(){
+/*fun main(){ //exo 2.1
     var i = 1
     while (i<22){
         println(i)
@@ -82,4 +82,94 @@ fun main(){
             break
         }
     }
+}*/
+
+
+
+/*fun main(){ //exo 2.2
+    while(true){
+        println("Entrer un nombre pair ou impair :")
+        val i = readln().toInt()
+        var jojo = if (i%2==0){
+            println(i)
+              i+2
+
+        }
+        else {
+            println(i)
+            i+3
+
+
+        }
+        println("Le nouveau nombre est : $jojo")
+
+        if(i==28){
+            println("Le nombre est 28 on s'arrete ")
+            break
+        }
+    }
+}*/
+
+/*fun main(){ //exo 2.3
+    println("Entrer un nombre:")
+    var i  = readln().toInt()
+    while(i % 5 ==0 ){
+        println(i)
+        i = i + 5
+        if(i==50){
+            break
+        }
+
+    }
+
+}*/
+/*fun main() {//exo 3
+    var tableau = arrayOf<Int>()
+    for(i in 1..10){
+        tableau += (1..100).random()
+    }
+    println(tableau.contentToString())
+    println(tableau.sum())
+    println(tableau.max())
+    println(tableau.min())
+
+    var tabpair = listOf<Int>()
+    var tabimpair= listOf<Int>()
+
+
+    for (element in tableau){
+        if (element % 2==0){
+            tabpair=tabpair.add
+        }
+        else {
+            tabimpair.
+        }
+
+        }
+}*/
+fun main(){//4.2
+    val i = 5
+    for ( i in i downTo 1 )
+        println(i)
+
+    var x = 5 //4.1
+    for (i in  1..x)
+        println(i)
+
+    var tabpair= intArrayOf(0,1,2,3,4,5,6,7,8,9,10)//exo4.3
+    for (tabpair in 0..10 step 2 )
+        print(tabpair)
+
+    var tabimpair= intArrayOf(0,1,2,3,4,5,6,7,8,9,10)//exo4.4
+    for (tabimpair in 0..10 step 3 )
+        print(tabimpair)
+
+
+
 }
+
+
+
+
+
+
