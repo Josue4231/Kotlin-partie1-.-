47ef7e1c807b95bc6c09dc0d1b63b73883dc6f15
Josue4231 <josuekialengela@gmail.com>
2025-03-07 09:43:02 +0100

    Add files via upload

---
+++ Kotlin - exercices.kt
@@ -0,0 +1,103 @@
+/*fun main(){
+    println("Entrer votre nom")
+    val name= readln()
+    println("Bonjour $name")
+}
+*/
+/*fun main(){
+    println("Entrer un nombre x :")
+    val x =  readln().toInt()
+    println("Entrer un nombre y :")
+    val y = readln().toInt()
+    println("somme: ${x+y}")
+    println("produit: ${x*y}")
+
+
+    if(x!=0 && x>0){
+        println("Quotient: ${x/y}")
+    }
+    else{
+        println("impossible")
+    }
+}*/
+
+/*fun main(){
+    println("Entrer votre age : ")
+    val age = readln().toInt()
+    if (age>26){
+        println("tarif augmente reduit")
+    }
+    else if (age<26){
+        println("tarif reduit")
+    }
+
+}*/
+/*fun main() {
+    println("entrer un nombre x :")
+    val x = readln().toInt()
+    println("Entrer un nombre y :")
+    val y = readln().toInt()
+    if (x < 0 && y > 0) {
+        println("produit négatif")
+    } else if (x > 0 && y > 0) {
+        println("produit positif")
+    } else {
+        println("produit nulle ")
+    }
+
+}*/
+
+/*fun main(){
+    println("Entrer une année : ")
+    val an = readln().toInt()
+    if (an%4==0 || an%100==0 && an%400==0){
+        println("Est une année bissextile")
+    }
+    else {
+        println("N'est pas une année bissextile ")
+    }
+}*/
+/*fun main(){ //exo 2.1
+    var i = 1
+    while (i<21){
+        println(i)
+        i=i+1
+        if (i==21){
+            break
+        }
+    }
+}*/
+/*fun main() {
+    println("Entrer un chiffre")
+    var i = readln().toInt()
+    while (i % 2 == 0 || i % 2 != 0) {
+        println(i)
+        i=i+2
+        if (i >= 28) {
+            break
+        }
+    }
+}*/
+/*fun main(){
+    println("Entrer un nombre")
+    var i = readln().toInt()
+    println("Entrer un autre nombre ")
+    var x = readln().toInt()
+    while(i%5==0){
+        println(i)
+        i+=5
+        if(i>=x) {
+            break
+
+        }
+    }
+}*/
+/*fun main() {
+    for (i in 1..5)
+        println(i)
+}*/
+
+
+
+
+
---
+++ kotclin_collection.kt
@@ -0,0 +1,38 @@
+/*fun main(){
+    val planetes = arrayOf("mercure", "Venus", "terre", "Mars", "Jupiter", "saturne",
+        "Uranus", "Neptune")
+    println(planetes[0])
+    println(planetes[7])
+    println(planetes.first())
+    println(planetes.last())
+     planetes [2]="La Terre"
+    println(planetes.filter { it.uppercase().startsWith('M') })
+    println(planetes.filter { it.lowercase().startsWith('m') })
+    println(planetes.filter { it.lowercase().startsWith('s') })
+    for (planetes in planetes)
+        println(planetes)
+
+
+}*/
+/*fun main() {
+    val moi = listOf(
+        "mercure", "Venus", "terre", "Mars", "Jupiter", "saturne",
+        "Uranus", "Neptune"
+    )
+    println(moi[0])
+    println("le meilleur:${moi[4]}")
+    println(moi.get(5))
+}*/
+/*fun main(){
+    val fruits = mutableListOf("cerise", "banane", "orange", "pomme", "papaye")
+    fruits.add("poire")
+    fruits.removeAt(2)
+    fruits.add(0,"fraise")
+    println(fruits)
+}*/
+
+
+
+
+
+
